eb6ee9f13db20b8374140c2d74c90d150f19d439
alexmirea23409-lab <alexmirea23409@gmail.com>
2026-07-25 19:44:20 +0300

    Update socials.js

--- api/socials.js
+++ api/socials.js
@@ -1,8 +1,15 @@
 export default async function handler(req, res) {
+
+  // Allow StreamElements (and other browsers) to access this API
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const API_KEY = process.env.YOUTUBE_API_KEY;
   const CHANNEL_ID = "UCdzf46Ahuj5-qk2c6D7YKJA";
 
   try {
+
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_ID}&key=${API_KEY}`
     );
@@ -19,13 +26,16 @@ export default async function handler(req, res) {
       data.items[0].statistics.subscriberCount
     );
 
-    res.status(200).json({
+    return res.status(200).json({
       youtube: subscribers
     });
 
   } catch (err) {
-    res.status(500).json({
+
+    return res.status(500).json({
       error: err.message
     });
+
   }
+
 }
